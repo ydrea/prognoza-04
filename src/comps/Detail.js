d612c6b1a8cab284dcdea77998db14dad88255fb
ydrea <ydrea.wrld@gmail.com>
2022-07-14 18:02:04 +0200

    list designed

--- src/comps/Detail.js
+++ src/comps/Detail.js
@@ -22,30 +22,20 @@ function Detail({ id, city, fav }) {
   };
 
   return (
-    <li className={`list-item ${fav && "list-item-success"}`}>
-      <div className="card">
-        <div className="card-inner">
-          <div className="card-front">
-            <h1 className="h1">{city.name}</h1>
-          </div>
-          <div className="card-back">
-            <h1>{city}</h1>
-            <ul>
-              <li>{/* <strong>Actor Name:</strong> {item.portrayed} */}</li>
-              <li>{/* <strong>Nickname:</strong> {item.nickname} */}</li>
-              <li>{/* <strong>Birthday:</strong> {item.birthday} */}</li>
-              <li>{/* <strong>Status:</strong> {item.status} */}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <span>
+    <div className="card">
+      <span className="command">
         <input type="checkbox" checked={fav} onChange={handleFavCheck} />
-        {city}
+
+        <h1 className="h1">{city}</h1>
+        <button onClick={handleDeleteIt}>X</button>
       </span>
-      <button onClick={handleDeleteIt}>X</button>
-    </li>
+      <ul className="ulist">
+        <li>{/* <strong>Actor Name:</strong> {item.portrayed} */}</li>
+        <li>{/* <strong>Nickname:</strong> {item.nickname} */}</li>
+        <li>{/* <strong>Birthday:</strong> {item.birthday} */}</li>
+        <li>{/* <strong>Status:</strong> {item.status} */}</li>
+      </ul>
+    </div>
   );
 }
 Detail.propTypes = {
